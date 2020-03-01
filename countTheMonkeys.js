156432227e6ad24578dd3c693734b4bf54df3189
Roberto Garcia <41040915+naranjito72@users.noreply.github.com>
2020-03-02 00:15:03 +0100

    Update countTheMonkeys.js

--- countTheMonkeys.js
+++ countTheMonkeys.js
@@ -1,3 +1,4 @@
+// 9/12/2019
 function monkeyCount(n) {
 let monky = [];
  for( let i = 1; i <= n; ++i){
@@ -7,3 +8,8 @@ return monky;
 };
 
 monkeyCount(10);
+
+// 15/02/2020
+const monkeyCount = (n) => [...Array(n)].map(_ => n++);
+monkeyCount(15);
+
